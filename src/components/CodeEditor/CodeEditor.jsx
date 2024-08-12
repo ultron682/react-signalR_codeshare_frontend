a3d8be62c96c95e7a198590b676f5585a904a247
ultron682 <52131708+ultron682@users.noreply.github.com>
2024-08-12 11:41:54 +0200

    separate connection from collaborativeEditor.jsx

--- src/components/CodeEditor/CodeEditor.jsx
+++ src/components/CodeEditor/CodeEditor.jsx
@@ -1,15 +1,8 @@
-import React, {
-  useState,
-  useEffect,
-  useContext,
-  useRef
-} from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams, useNavigate } from "react-router-dom";
 
 import * as signalR from "@microsoft/signalr";
-// import { Controlled as CodeMirror } from "react-codemirror2";
-
 
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
@@ -25,33 +18,32 @@ import "codemirror/mode/swift/swift.js";
 
 import { useTheme } from "../ThemeContext";
 import "./CodeEditor.css";
-import { AuthContext } from "../AuthContext";
-import LoadingPopup from "../LoadingPopup";
+// import { AuthContext } from "../AuthContext";
+// import LoadingPopup from "../LoadingPopup";
 import CodeDownloader from "./CodeDownloader";
 import { BounceLoader } from "react-spinners";
-
-import CollaborativeEditor from './CollaborativeEditor';
-
+import CollaborativeEditor from "./CollaborativeEditor";
 
 const CodeEditor = () => {
   const { t } = useTranslation();
   const { theme } = useTheme();
 
-  const [codeContent, setCodeContent] = useState("");
+  const [documentContent, setDocumentContent] = useState("");
+  const documentContentRef = useRef(documentContent);
+  documentContentRef.current = documentContent;
 
   const [connection, setConnection] = useState(null);
-  const connectionRef = useRef(null);
+
+  const isServerChangeRef = useRef(false);
 
   const [uniqueId, setUniqueId] = useState("temp");
   const [languageProg, setLanguageProg] = useState("javascript");
   const [isConnected, setIsConnected] = useState(true);
   const [isSaved, setIsSaved] = useState(true);
-  const { user } = useContext(AuthContext);
+  // const { user } = useContext(AuthContext);
 
   const { id } = useParams();
   const navigate = useNavigate();
-
-
 
   const optionsLang = [
     "javascript",
@@ -71,77 +63,136 @@ const CodeEditor = () => {
     }
   }, [id, navigate]);
 
-  // useEffect(() => {
-  //   const newConnection = new signalR.HubConnectionBuilder()
-  //     .withUrl("http://localhost:5555/codesharehub", {
-  //       withCredentials: false,
-  //     })
-  //     .withAutomaticReconnect()
-  //     .build();
+  useEffect(() => {
+    const connect = async () => {
+      const newConnection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:5555/codesharehub", {
+          withCredentials: false,
+        })
+        .withAutomaticReconnect()
+        .build();
 
-  //   setConnection(newConnection);
-  // }, []);
+      setConnection(newConnection);
+    };
 
-  // useEffect(() => {
-  //   if (
-  //     connection &&
-  //     connection.state === signalR.HubConnectionState.Disconnected
-  //   ) {
-  //     connection
-  //       .start()
-  //       .then(() => {
-  //         console.log("Connected!");
+    connect();
+  }, []);
 
-  //         // connection.on("ReceiveUpdate", (update) => {
-  //         //   const changes = ChangeSet.fromJSON(update.changes);
-  //         //   receiveUpdates(view, [changes]);
-  //         //   version = update.version;
-  //         // });
+  useEffect(() => {
+    if (
+      connection &&
+      connection.state === signalR.HubConnectionState.Disconnected
+    ) {
+      connection
+        .start()
+        .then(() => {
+          console.log("Connected!");
 
-  //         // connection.on("InitialState", (data) => {
-  //         //   const updates = data.updates.map((update) =>
-  //         //     ChangeSet.fromJSON(update.changes)
-  //         //   );
-  //         //   version = data.version;
-  //         //   receiveUpdates(view, updates);
-  //         // });
+          connection.on("ReceiveDocument", (doc, changes) => {
+            setDocumentContent(doc);
+          });
 
-  //         // connectionRef.current = connection;
+          connection.on("ReceiveUpdate", (changeSetJson) => {
+            isServerChangeRef.current = true;
 
-  //         // connection.invoke("GetInitialState");
-  //       })
-  //       .catch((e) => {
-  //         setIsConnected(false);
-  //         // console.log("Connection failed: ", e);
-  //       });
+            const changeSet = JSON.parse(changeSetJson);
 
-  //     connection.onreconnected(() => {
-  //       console.log("Connection reestablished.");
-  //       setIsConnected(true);
-  //     });
+            const updatedDoc = applyChangeSet(
+              documentContentRef.current,
+              changeSet
+            );
 
-  //     connection.onclose(() => {
-  //       console.log("Connection closed.");
-  //       setIsConnected(false);
-  //     });
+            console.log(
+              "updatedDoc:",
+              updatedDoc,
+              "changeSetJson: ",
+              changeSetJson
+            );
 
-  //     connection.onreconnecting(() => {
-  //       console.log("Connection lost, reconnecting.");
-  //       setIsConnected(false);
-  //     });
-  //   }
-  //   return () => {
-  //     if (connection) {
-  //       console.log("Connection closed ReceivedNewLineCode.");
-  //       connection.off("ReceivedNewLineCode");
-  //     }
-  //   };
-  // }, [connection, uniqueId]);
+            console.log(3);
+
+            setDocumentContent(updatedDoc);
+          });
+
+          connection.invoke("JoinDocument", uniqueId);
+          connection.invoke("SubscribeDocument", uniqueId);
+
+          setConnection(connection);
+        })
+        .catch((e) => {
+          setIsConnected(false);
+          // console.log("Connection failed: ", e);
+        });
+
+      connection.onreconnected(() => {
+        console.log("Connection reestablished.");
+        setIsConnected(true);
+      });
+
+      connection.onclose(() => {
+        console.log("Connection closed.");
+        setIsConnected(false);
+      });
+
+      connection.onreconnecting(() => {
+        console.log("Connection lost, reconnecting.");
+        
+        setIsConnected(false);
+      });
+    }
+    return () => {
+      if (connection) {
+        console.log("Connection closed ReceivedNewLineCode.");
+        connection.off("ReceiveUpdate");
+        connection.off("ReceiveDocument");
+        connection.invoke("UnsubscribeDocument", uniqueId);
+        
+      }
+    };
+  }, [connection, uniqueId]);
 
   const handleLanguageProgChange = (event) => {
     setLanguageProg(event.target.value);
   };
 
+  const applyChangeSet = (doc, changeSet) => {
+    let newChanges = undefined;
+
+    if (changeSet.Text === "") {
+      newChanges =
+        doc.substring(0, changeSet.Start) +
+        changeSet.Text +
+        doc.substring(changeSet.Start + changeSet.Length);
+    } else {
+      newChanges =
+        doc.substring(0, changeSet.Start) +
+        changeSet.Text +
+        doc.substring(changeSet.Start + changeSet.Length);
+    }
+    //console.log("applyChangeSet", doc, changeSet, newChanges);
+
+    return newChanges;
+  };
+
+  const handleEditorChange = (editor, data, value) => {
+    //console.log("handleEditorChange", isServerChangeRef.current);
+    if (isServerChangeRef.current === true) return;
+
+    if (connection) {
+      const start = editor.indexFromPos(data.from);
+      const end = editor.indexFromPos(data.to);
+      const length = end - start;
+
+      const changeSet = {
+        Start: start,
+        Length: length,
+        Text: data.text.join("\n"),
+      };
+
+      connection.invoke("PushUpdate", uniqueId, JSON.stringify(changeSet));
+      console.log("handleEditorChange", JSON.stringify(changeSet));
+    }
+  };
 
   return (
     <div className="CodeEditorContainer">
@@ -163,7 +214,10 @@ const CodeEditor = () => {
           ))}
         </select>
 
-        <CodeDownloader filename={uniqueId} data={codeContent}></CodeDownloader>
+        <CodeDownloader
+          filename={uniqueId}
+          data={documentContent}
+        ></CodeDownloader>
 
         {<BounceLoader loading={!isSaved} size="20px" color="white" />}
       </div>
@@ -177,7 +231,14 @@ const CodeEditor = () => {
         </div>
       )}
 
-     <CollaborativeEditor languageProg={languageProg} documentId={uniqueId} theme={theme}></CollaborativeEditor>
+      <CollaborativeEditor
+        languageProg={languageProg}
+        documentContent={documentContent}
+        setDocumentContent={setDocumentContent}
+        theme={theme}
+        onHandleEditorChange={handleEditorChange}
+        isConnected={isConnected}
+      ></CollaborativeEditor>
     </div>
   );
 };
--- src/components/CodeEditor/CollaborativeEditor.jsx
+++ src/components/CodeEditor/CollaborativeEditor.jsx
@@ -1,113 +1,17 @@
-import React, { useState, useEffect, useRef } from "react";
-import { HubConnectionBuilder } from "@microsoft/signalr";
+import React, { useRef } from "react";
 import { Controlled as CodeMirror } from "react-codemirror2";
 import "codemirror/lib/codemirror.css";
 import "codemirror/mode/javascript/javascript";
 
-const CollaborativeEditor = ({ documentId, languageProg, theme }) => {
-  const [connection, setConnection] = useState(null);
-  const [documentContent, setDocumentContent] = useState("");
-  const documentContentRef = useRef(documentContent);
-  documentContentRef.current = documentContent;
-
-  const isServerChangeRef = useRef(false);
-
+const CollaborativeEditor = ({
+  documentContent,
+  setDocumentContent,
+  languageProg,
+  theme,
+  onHandleEditorChange,
+  isConnected
+}) => {
   const editorRef = useRef(null);
-
-  useEffect(() => {
-    const connect = async () => {
-      const newConnection = new HubConnectionBuilder()
-        .withUrl("http://localhost:5555/codesharehub", {
-          withCredentials: false,
-        })
-        .withAutomaticReconnect()
-        .build();
-
-      newConnection.on("ReceiveDocument", (doc, changes) => {
-        setDocumentContent(doc);
-      });
-
-      newConnection.on("ReceiveUpdate", (changeSetJson) => {
-        isServerChangeRef.current = true;
-
-        const changeSet = JSON.parse(changeSetJson);
-
-        const updatedDoc = applyChangeSet(
-          documentContentRef.current,
-          changeSet
-        );
-
-        console.log(
-          "updatedDoc:",
-          updatedDoc,
-          "changeSetJson: ",
-          changeSetJson
-        );
-
-        console.log(3);
-        setDocumentContent(updatedDoc);
-      });
-
-      await newConnection.start();
-      await newConnection.invoke("JoinDocument", documentId);
-      await newConnection.invoke("SubscribeDocument", documentId);
-
-      setConnection(newConnection);
-    };
-
-    connect();
-
-    return () => {
-      if (connection) {
-        connection.invoke("UnsubscribeDocument", documentId);
-        connection.stop();
-      }
-    };
-    // eslint-disable-next-line
-  }, [documentId]);
-
-  useEffect(() => {// TESTED AND DONT NEED
-    isServerChangeRef.current = false;
-  }, [documentContent]); 
-
-  const applyChangeSet = (doc, changeSet) => {
-    let newChanges = undefined;
-
-    if (changeSet.Text === "") {
-      newChanges = doc.substring(0, changeSet.Start) +
-      changeSet.Text +
-      doc.substring(changeSet.Start + changeSet.Length);;
-    } else {
-      newChanges =
-        doc.substring(0, changeSet.Start) +
-        changeSet.Text +
-        doc.substring(changeSet.Start + changeSet.Length);
-    }
-    //console.log("applyChangeSet", doc, changeSet, newChanges);
-
-    return newChanges;
-  };
-
-  // only fully executed when current client makes a new change
-  const handleEditorChange = (editor, data, value) => {
-    //console.log("handleEditorChange", isServerChangeRef.current);
-    if (isServerChangeRef.current === true) return;
-
-    if (connection) {
-      const start = editor.indexFromPos(data.from);
-      const end = editor.indexFromPos(data.to);
-      const length = end - start;
-
-      const changeSet = {
-        Start: start,
-        Length: length,
-        Text: data.text.join("\n"),
-      };
-
-      connection.invoke("PushUpdate", documentId, JSON.stringify(changeSet));
-      console.log("handleEditorChange", JSON.stringify(changeSet));
-    }
-  };
 
   return (
     <CodeMirror
@@ -118,36 +22,15 @@ const CollaborativeEditor = ({ documentId, languageProg, theme }) => {
         theme: theme === "light" ? "material" : "material-darker",
         lineNumbers: true,
         lineWrapping: true,
-        // readOnly: !isConnected ? "nocursor" : false,
+         readOnly: !isConnected ? "nocursor" : false,
       }}
       onBeforeChange={(editor, data, value) => {
         console.log(1);
         setDocumentContent(value);
-        handleEditorChange(editor, data, value);
+        onHandleEditorChange(editor, data, value);
       }}
     />
   );
 };
 
 export default CollaborativeEditor;
-
-
-/*
-<CodeMirror
-        value={codeContent.code}
-        options={{
-          mode: languageProg,
-          theme: theme === "light" ? "material" : "material-darker",
-          lineNumbers: true,
-          lineWrapping: true,
-          readOnly: !isConnected ? "nocursor" : false,
-        }}
-        onBeforeChange={(editor, metadata, value) => {
-          setCodeContent({ code: value, fromOtherUser: false });
-        }}
-        onChange={handleEditorChange}
-        minHeight="100%"
-        height="100%"
-      />
-
-*/
