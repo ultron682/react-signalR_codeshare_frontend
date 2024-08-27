a5c3b7cba15b971fd8a54afe2c22983e6d6a251a
ultron682 <52131708+ultron682@users.noreply.github.com>
2024-08-27 19:49:17 +0200

    works without handle

--- src/components/CodeEditor/CollaborativeEditor.jsx
+++ src/components/CodeEditor/CollaborativeEditor.jsx
@@ -1,17 +1,23 @@
-import React, { useRef } from "react";
-import { Controlled as CodeMirror } from "react-codemirror2";
+import React from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { xml } from "@codemirror/lang-xml";
+import { css } from "@codemirror/lang-css";
+import { go } from "@codemirror/lang-go";
+import { php } from "@codemirror/lang-php";
+import { python } from "@codemirror/lang-python";
+import { sql } from "@codemirror/lang-sql";
+import { dracula, draculaDarkStyle } from "@uiw/codemirror-theme-dracula"; // Przykładowe tematy
 
-import "codemirror/lib/codemirror.css";
-import "codemirror/theme/material.css";
-import "codemirror/theme/material-darker.css";
-import "codemirror/mode/javascript/javascript.js";
-import "codemirror/mode/xml/xml.js";
-import "codemirror/mode/css/css.js";
-import "codemirror/mode/go/go.js";
-import "codemirror/mode/php/php.js";
-import "codemirror/mode/python/python.js";
-import "codemirror/mode/sql/sql.js";
-import "codemirror/mode/swift/swift.js";
+const languageModes = {
+  javascript: javascript(),
+  xml: xml(),
+  css: css(),
+  go: go(),
+  php: php(),
+  python: python(),
+  sql: sql(),
+};
 
 const CollaborativeEditor = ({
   documentContent,
@@ -21,23 +27,20 @@ const CollaborativeEditor = ({
   onHandleEditorChange,
   isConnected,
 }) => {
-  const editorRef = useRef(null);
+  const handleEditorChange = (value, viewUpdate) => {
+    setDocumentContent(value);
+    console.log("handleEditorChange", viewUpdate);
+    onHandleEditorChange(value, viewUpdate);
+  };
 
   return (
     <CodeMirror
-      ref={editorRef}
       value={documentContent}
-      options={{
-        mode: languageProg,
-        theme: theme === "light" ? "material" : "material-darker",
-        lineNumbers: true,
-        lineWrapping: true,
-        readOnly: !isConnected ? "nocursor" : false
-      }}
-      onBeforeChange={(editor, data, value) => {
-        setDocumentContent(value);
-        onHandleEditorChange(editor, data, value);
-      }}
+      height="100%"
+      // theme={theme === "light" ? draculaDarkStyle : dracula} // Możesz zmienić na inne dostępne tematy
+      // extensions={[languageModes[languageProg]]}
+      onChange={handleEditorChange}
+      editable={isConnected}
     />
   );
 };
